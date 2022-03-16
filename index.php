1342cf5814ab7cd1c885c8c1401e1f3d478d1e6f
Shayak Rakshit <85937567+iamgithum@users.noreply.github.com>
2022-03-16 20:50:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,7 @@
   <head>
     <title>Blog Site</title>
     <meta name="Blog Site" content="HTML/ CSS/ Js/ PHP">
-    <style rel="stylesheet" type="text/css">
-      body{
-        margin:9px;
-        padding:9px;
-        background-color:#191919;
-      }
-    </style>
+    <link rel="stylesheet" type="text/css" href="design.css">
   </head>
   <body>
   </body>
