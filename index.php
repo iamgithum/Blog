2826648d4c8347733ca328a411be358e2f1efeb9
Markzmania <101707594+markzmania@users.noreply.github.com>
2022-03-16 16:39:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
     <title>Blog Site</title>
     <meta name="Blog Site" content="HTML/ CSS/ Js/ PHP">
     <style rel="stylesheet" type="text/css">
+      body{
+        margin:9px;
+        padding:9px;
+        background-color:#191919;
+      }
+    </style>
   </head>
   <body>
   </body>
