a3346409a8d3ba0c9307312c54d3d2c168f6d0c3
Markzmania <101707594+markzmania@users.noreply.github.com>
2022-03-16 16:05:49 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html lang=en>
+  <head>
+    <title>Blog Site</title>
+    <meta name="Blog Site" content="HTML/ CSS/ Js/ PHP">
+    <style rel="stylesheet" type="text/css">
+  </head>
+  <body>
+  </body>
+</html>
