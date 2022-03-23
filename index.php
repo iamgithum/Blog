add515936490216b0909efa31c9be7a11cd282dc
Shayak Rakshit <85937567+iamgithum@users.noreply.github.com>
2022-03-23 16:28:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,106 @@
-<html lang=en>
-  <head>
-    <title>Blog Site</title>
-    <meta name="Blog Site" content="HTML/ CSS/ Js/ PHP">
-    <link rel="stylesheet" type="text/css" href="design.css">
-  </head>
-  <body>
-  </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog | Mzm Project</title>
+    <link rel="stylesheet" href="mzm_design.css">
+</head>
+<body>
+    <header>
+        <h1>Markzmania Blog Project</h1>
+    </header>
+    <nav>
+        <a href="#">Home</a><span class="nav"> | </span>
+        <a href="#">Blogs</a><span class="nav"> | </span>
+        <a href="#">About</a><span class="nav"> | </span>
+        <a href="#">Contact</a>
+    </nav>
+    <main>
+        <section>
+            <div id="us">
+                <img src="Media/image/IMG20220213135507.jpg" width="100%">
+                <h2>Markzmania welcomes you, to join the family.</h2>
+                <hr noshade width="27px" id="hr">
+            </div>
+        </section>
+        <section>
+            <div id="iius">
+                <center>
+                    <img src="Media/image/IMG20220213130524.jpg" width="20%">
+                    <img src="Media/image/IMG20220213130626(1).jpg" width="20%">
+                    <img src="Media/image/IMG20220213133124.jpg" width="20%">
+                    <img src="Media/image/indedx.jpeg" width="20%">
+                    <table border="0" width="82%">
+                        <tr width="80%">
+                            <td width="25%" align="center">Rohit K. Maurya</td>
+                            <td width="25%" align="center">Sneh Pratap Singh</td>
+                            <td width="25%" align="center">Piyush Rai</td>
+                            <td width="25%" align="center">Shubham K. Maurya</td>
+                        </tr>
+                    </table>
+                </center>
+            </div>
+        </section>
+        <section>
+            <table style="padding:9px;">
+                <tr>
+                    <td>
+                        <article style="padding:10px; ">
+                            <h2>A Heading</h2>
+                            <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quibusdam qui, voluptas in mollitia nobis placeat. Quaerat praesentium, quisquam laudantium ipsa dicta maxime odio, non tempore voluptatum, perspiciatis blanditiis at ab veniam harum recusandae laboriosam sequi fugiat corrupti et est nam reprehenderit eum ipsam. Exercitationem, ad dolorem molestias quos numquam fugit et animi rem repellat vitae quasi dolorum aliquam est reprehenderit? Eaque fugit perferendis incidunt alias voluptates provident itaque magni corrupti eius non fugiat magnam architecto voluptatibus illo, rem quisquam nesciunt consequatur voluptatum nulla error. Repudiandae sapiente mollitia perferendis, sint commodi repellendus quibusdam unde ipsam adipisci velit architecto quidem expedita magni veritatis eius fugit impedit reiciendis quam! Exercitationem illo maxime sed dolorem deleniti aut reprehenderit, perferendis aspernatur esse earum debitis, error laboriosam inventore dolore. Vel, eligendi molestias voluptate dolores deserunt illo ex error ratione consectetur. Autem maiores provident dolore placeat sit natus ipsa enim tenetur commodi reprehenderit sunt et qui, incidunt quibusdam modi! Rem vel corrupti iste, sequi similique, magnam voluptatum placeat soluta nulla sint illum ex sapiente culpa officiis? Ex sint suscipit obcaecati natus consequatur perspiciatis. Quis in velit nulla voluptate laboriosam totam asperiores saepe iure repudiandae tenetur. Quam commodi nostrum nam ducimus. Explicabo ea sint nisi odio dignissimos.</p>
+                            </article>
+                    </td>
+                    <td>
+                        <article style="padding:6px; background-color:#191919; color: white; margin:6px; border-radius:3px; margin-top:99px;">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla dolorum necessitatibus temporibus earum beatae fugit autem commodi rem, repellat officiis mollitia dolorem nisi, tenetur, deleniti ad repellendus? Sed earum enim pariatur officia, saepe optio nostrum atque. Ipsum numquam tenetur libero praesentium ducimus itaque dolor minus dolores aliquam, vero optio explicabo!</p>
+                        </article>
+                    </td>
+                </tr>
+            </table>            
+        </section>
+        <section id="sec">
+            <h2 style="color:antiquewhite;">Contact Us</h2>
+            <table align="center" cellspacing="36" cellpadding="36">
+                <tr>
+                    <td>
+                        <img id="img" src="Media/image/IMG_20211023_221156_232.jpg" height="540">
+                    </td>
+                    <td>
+                        <form method="post" action="server.php">
+                            <fieldset>
+                                <legend>Name</legend>
+                                <input type="text" name="name" id="name" class="form" placeholder="Your Name">
+                            </fieldset><br>
+                            <fieldset>
+                                <legend>Email</legend>
+                                <input type="email" name="email" id="email" class="form" placeholder="Your Email">
+                            </fieldset>
+                            <br>
+                            <fieldset>
+                                <legend>Phone</legend>
+                                <input type="tel" name="phone" id="phone" class="form" placeholder="Your Phone">
+                            </fieldset><br>
+                            <fieldset>
+                                <legend>Message</legend>
+                                <textarea name="Message" id="Message" cols="24" rows="5" class="form" placeholder="Your Message"></textarea>
+                            </fieldset><br><br>
+                            <table width="5%">
+                                <tr>
+                                    <td style="width:auto;" align="center"><input type="submit" value="Submit" class="form"></td>
+                                    <td style="width:auto; font-size:9px; color:slateblue;">hhhhhhhhhhhhhhhhhh</td>
+                                    <td style="width:auto;" align="center"><input type="reset" value="Reset" class="form"></td>
+                                </tr>
+                            </table>
+                        </form>
+                    </td>
+                </tr>
+            </table>
+        </section>
+    </main>
+    <footer>
+        <p> &copy; All rights reserved 2021 | <a style="color:black" href="https://markzmania.com" target="_blank">Markzmania</a>  </p>
+    </footer>
+</body>
 </html>
